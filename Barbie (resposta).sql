64746bf097c222a66b3435f177a3ed9c4b3e7d24
Leonardo Moura de Sousa <leonardomourass2008@gmail.com>
2026-05-23 20:40:42 -0300

    questoes dificeis

--- Barbie (resposta).sql
+++ Barbie (resposta).sql
@@ -2,53 +2,29 @@
 
 -- 1
 
-SELECT
-    o.nome AS objeto,
-    o.categoria,
-    o.elegancia,
-    c.nome AS closet,
-    p.nome AS personagem,
-    e.nome AS evento,
-    e.data_evento
-FROM objeto o
-JOIN closet c
-    ON o.fkCloset = c.idCloset
-JOIN personagem_objeto po
-    ON o.idObjeto = po.fkObjeto
-JOIN personagem p
-    ON po.fkPersonagem = p.idPersonagem
-JOIN evento_objeto eo
-    ON o.idObjeto = eo.fkObjeto
-JOIN evento e
-    ON eo.fkEvento = e.idEvento
+SELECT o.nome AS objeto,o.categoria,o.elegancia,c.nome AS closet,
+    p.nome AS personagem,e.nome AS evento,e.data_evento FROM objeto o
+JOIN closet c ON o.fkCloset = c.idCloset
+JOIN personagem_objeto po ON o.idObjeto = po.fkObjeto
+JOIN personagem p ON po.fkPersonagem = p.idPersonagem
+JOIN evento_objeto eo ON o.idObjeto = eo.fkObjeto
+JOIN evento e ON eo.fkEvento = e.idEvento
 WHERE e.nome = 'Golden Prestige Dinner'
 AND o.disponivel = TRUE
 AND o.exclusivo = FALSE
 AND o.data_liberacao <= DATE(e.data_evento)
 AND o.elegancia >= e.dress_code_min
-AND p.nome IN (
-    'Barbie',
-    'Skipper Celestine',
-    'Stacie Aurora',
-    'Chelsea Elodie'
-)
+AND p.nome IN ('Barbie','Skipper Celestine','Stacie Aurora','Chelsea Elodie')
 ORDER BY o.elegancia DESC
 LIMIT 3;
 
 -- 2
 
-SELECT
-    p.nome,
-    COUNT(o.idObjeto) AS total_objetos
-FROM personagem p
-JOIN personagem_objeto po
-    ON p.idPersonagem = po.fkPersonagem
-JOIN objeto o
-    ON po.fkObjeto = o.idObjeto
-JOIN evento_objeto eo
-    ON o.idObjeto = eo.fkObjeto
-JOIN evento e
-    ON eo.fkEvento = e.idEvento
+SELECT p.nome, COUNT(o.idObjeto) AS total_objetos FROM personagem p
+JOIN personagem_objeto po ON p.idPersonagem = po.fkPersonagem
+JOIN objeto o ON po.fkObjeto = o.idObjeto
+JOIN evento_objeto eo ON o.idObjeto = eo.fkObjeto
+JOIN evento e ON eo.fkEvento = e.idEvento
 WHERE e.nome = 'Golden Prestige Dinner'
 AND o.disponivel = TRUE
 AND o.exclusivo = FALSE
@@ -59,16 +35,10 @@ ORDER BY total_objetos DESC;
 
 -- 3
 
-SELECT
-    c.nome,
-    COUNT(o.idObjeto) AS total_objetos
-FROM closet c
-JOIN objeto o
-    ON c.idCloset = o.fkCloset
-JOIN evento_objeto eo
-    ON o.idObjeto = eo.fkObjeto
-JOIN evento e
-    ON eo.fkEvento = e.idEvento
+SELECT c.nome, COUNT(o.idObjeto) AS total_objetos
+FROM closet c JOIN objeto o ON c.idCloset = o.fkCloset
+JOIN evento_objeto eo ON o.idObjeto = eo.fkObjeto
+JOIN evento e ON eo.fkEvento = e.idEvento
 WHERE e.nome = 'Golden Prestige Dinner'
 AND o.disponivel = TRUE
 AND o.exclusivo = FALSE
@@ -79,8 +49,7 @@ HAVING COUNT(o.idObjeto) > 2;
 
 -- 4
 
-SELECT nome
-FROM closet
+SELECT nome FROM closet
 WHERE tipo_acesso = 'Premium'
 AND idCloset NOT IN (
     SELECT o.fkCloset
@@ -98,14 +67,9 @@ AND idCloset NOT IN (
 
 -- 5
 
-SELECT
-    o.categoria,
-    COUNT(o.idObjeto) AS total_objetos
-FROM objeto o
-JOIN evento_objeto eo
-    ON o.idObjeto = eo.fkObjeto
-JOIN evento e
-    ON eo.fkEvento = e.idEvento
+SELECT o.categoria, COUNT(o.idObjeto) AS total_objetos
+FROM objeto o JOIN evento_objeto eo ON o.idObjeto = eo.fkObjeto
+JOIN evento e ON eo.fkEvento = e.idEvento
 WHERE e.nome = 'Golden Prestige Dinner'
 AND o.disponivel = TRUE
 AND o.exclusivo = FALSE
